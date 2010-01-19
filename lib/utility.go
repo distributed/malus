1ab0d01553f0ba5e7b18380554f3de39853c7a3b
Michael Meier <distributed@gloenggi.ch>
2010-01-19 20:21:11 +0100

    created distance type

--- lib/utility.go
+++ lib/utility.go
@@ -3,8 +3,16 @@ package malus
 import (
 	"crypto/sha1"
 	"strings"
+	"encoding/hex"
 )
 
+
+type Distance []byte;
+
+
+func (d Distance) String() string {
+	return hex.EncodeToString(d)
+}
 func SHA1Bytes(b []byte) string {
 	h := sha1.New()
 	h.Write(b)
